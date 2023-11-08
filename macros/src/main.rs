274b0c91716ce49f5726679410a728d051df9dfc
gwdiii <georgedanforth@proton.me>
2023-11-08 16:01:02 -0600

    adds macros

---
+++ macros/src/main.rs
@@ -0,0 +1,32 @@
+use proc_macro;
+use macros::HelloMacro;
+
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+// #[some_attribute]
+// pub fn some_name(input: TokenStream) -> TokenStream {
+
+// }
+
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
+
+fn main() {
+    Pancakes::hello_macro();
+}
